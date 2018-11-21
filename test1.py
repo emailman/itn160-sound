98657ae6ef7a408091a64c56c96b08b29cc9a2e8
Eric Mailman <eric.mailman@yahoo.com>
2018-11-21 17:22:55 -0500

    Initial commit

---
+++ test1.py
@@ -0,0 +1,12 @@
+# import guizero
+# from pygame import mixer
+from time import sleep
+from playsound import playsound
+
+# mixer.init()
+# mixer.music.load('buzzer.wav')
+# mixer.music.play()
+
+sleep(1)
+
+playsound('buzzer.wav')
